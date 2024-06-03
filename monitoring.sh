609f26b89529bc5a65ff7c725deb0892069eb25c
Aurelien Focant <afocant@student.s19.begit config --global user.name Aurelien Focant>
2024-06-03 16:51:28 +0200

    done monitoring on disk usage

--- monitoring.sh
+++ monitoring.sh
@@ -6,11 +6,16 @@ VCPU=$(grep '^processor'   /proc/cpuinfo | uniq | wc -l)
 
 TOTARAM=$(free -m | grep Mem | awk '{print $2}')
 USEDRAM=$(free -m | grep Mem | awk '{print $3}')
-PERCRAM=$(free    | grep Mem | awk '{printf ("%.2f"), $3/$2*100 }')
+PERCRAM=$(free    | grep Mem | awk '{printf ("%.2f"), $3/$2*100}')
+
+TOTADISK=$(df -BG | awk '{td += $2} END {print td}')
+USEDDISK=$(df -BG | awk '{ud += $3} END {print ud}')
+PERCDISK=$(df -BG | awk '{td += $2} {ud += $3} END {printf ("%.2f"), ud/td*100}')
 
 echo	"
 Architecture:			$ARCH
 Nb of physical processors:	$PCPU
 Nb of  virtual processors:	$VCPU
-RAM usage:			${USEDRAM}/${TOTARAM} MiB = ${PERCRAM}%	
+RAM  usage:			${USEDRAM}/${TOTARAM} MiB = ${PERCRAM}%	
+DISK usage:			${USEDDISK}/${TOTADISK} GiB = ${PERCDISK}%
 "
